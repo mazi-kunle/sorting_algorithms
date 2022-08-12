afd56d8d681fe14ec1f8f14dd594c4544901a389
mazi-kunle <olakunlemichael644@gmail.com>
2022-08-11 19:32:32 -0700

    2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,24 +11,27 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp, swapped;
 
-	for (i = 0; i < size; i++)
+	if (size >= 2)
 	{
-		swapped = 0;
+		for (i = 0; i < size; i++)
+		{
+			swapped = 0;
 
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - i - 1; j++)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
-				swapped = 1;
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+					swapped = 1;
+				}
 			}
-		}
-		if (!swapped)
-		{
-			break;
+			if (!swapped)
+			{
+				break;
+			}
 		}
 	}
 }
